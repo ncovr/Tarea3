b0fa1451cab41c4dce29b90d85029595e69f8f4c
ncovr <nicolas.verdugo2301@alumnos.ubiobio.cl>
2025-06-28 01:08:18 -0400

    Commit message

--- src/GDP.java
+++ src/GDP.java
@@ -1,11 +1,9 @@
 // Grafo no dirigido ponderado
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedList;
 
-public class GNDP {
+public class GDP {
     class Arista {
         int id;
         String serial;
@@ -21,8 +19,8 @@ public class GNDP {
     }
 
     /**
-     * Esta es la clase que representa un grafo no dirigido ponderado, el cual funciona como un grafo común y corriente
-     * pero al aplicar operaciones se interpretan los nodos como personas, utilizando 'personas' a modo de diccionario
+     * Esta es la clase que representa un grafo dirigido ponderado, el cual funciona como un grafo común y corriente
+     * pero al aplicar operaciones se interpretan los nodos como personas, usando 'personas' a modo de diccionario
      */
 
     private Persona[] personas = new Persona[5]; // Diccionario de personas
@@ -30,8 +28,9 @@ public class GNDP {
     private int idCounter = 1; // Ayuda a generar Id's en orden correlativo según se van creando nuevas personas
 
     /**
-     * Por ejemplo, supongamos que existe una conexión entre '1' y '2'. Esto significa, usando el diccionario de personas,
-     * que la persona con Id = 1 se relaciona con la persona con Id = 2.
+     * Por ejemplo, supongamos que existe una conexión entre '1' y '2', donde el peso (serial) finaliza en 0.
+     * Esto significa, usando el diccionario de personas, que la persona con Id = 1 y persona con Id = 2 son amigos
+     * en una fecha determinada
      *
      * El peso de la arista es una serial generada automáticamente según los parámetros otorgados al crear la persona.
      * La estructura de la serial sería que los primeros 8 carácteres representan la fecha tipo DíaMesAño y el noveno
@@ -40,15 +39,20 @@ public class GNDP {
      */
 
     /**
-     * Lo anterior, viéndolo a partir de la estructura en código: si tenemos grafo[1] = {(2, 010120190), (3, 010120231)}
-     * significaria que la persona id = 1 se hizo amiga de persona id = 2 el 01012019 y bloqueó a la persona id = 3 el
+     * Lo anterior, viéndolo a partir de la estructura del código: si tenemos grafo[1] = {(2, 010120190), (3, 010120231)}
+     * significaria que la persona id = 1 se hizo amiga de persona id = 2 el 01012019 y que bloqueó a la persona id = 3 el
      * 01012023
      */
 
-    public GNDP() {
+    /**
+     * Dado que el grafo es dirigido, cuando se inicia una amistad, se crea una conexión bidireccional, pero cuando p1
+     * bloquea a p2, se crea esa conexión unidireccional, y luego p2 ya no está conectado con p1
+     */
+
+    public GDP() {
     }
 
-    public void insertarPersona(String nombre, int dia, int mes, String ocupacion, String email) {
+    public void crearPersona(String nombre, int dia, int mes, String ocupacion, String email) {
         Persona p = new Persona(idCounter, nombre, dia, mes, ocupacion, email);
         personas[idCounter] = p;
         idCounter++;
@@ -63,7 +67,7 @@ public class GNDP {
      * peso, donde el peso es la serial (fecha más digito de estado: amistad/bloqueo)
      */
     public void amigar(int p1, int p2, String fecha) {
-
+        fecha = fecha+"0";
     }
 
     /**
@@ -71,6 +75,7 @@ public class GNDP {
      * es un estado bidireccional o solo poner que p1 bloquea a p2 y que p2 ya no es amigo de p1
      */
     public void bloquear(int p1, int p2, String fecha) {
+        fecha = fecha+"1";
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        GNDP g = new GNDP();
-        g.insertarPersona("Sis", 9, 8, "Hola", "yolooo@gmail.com");
-        g.insertarPersona("Sis", 9, 8, "Hola", "yolooo@gmail.com");
-        g.insertarPersona("Sis", 9, 8, "Hola", "yolooo@gmail.com");
-        g.insertarPersona("Sis", 9, 8, "Hola", "yolooo@gmail.com");
-        g.insertarPersona("Sis", 9, 8, "Hola", "yolooo@gmail.com");
-        g.insertarPersona("Sis", 9, 8, "Hola", "yolooo@gmail.com");
+        GDP g = new GDP();
+        g.crearPersona("Sis", 9, 8, "Hola", "yolooo@gmail.com");
+        g.crearPersona("Sis", 9, 8, "Hola", "yolooo@gmail.com");
+        g.crearPersona("Sis", 9, 8, "Hola", "yolooo@gmail.com");
+        g.crearPersona("Sis", 9, 8, "Hola", "yolooo@gmail.com");
+        g.crearPersona("Sis", 9, 8, "Hola", "yolooo@gmail.com");
+        g.crearPersona("Sis", 9, 8, "Hola", "yolooo@gmail.com");
         g.getPersonas();
     }
 }
